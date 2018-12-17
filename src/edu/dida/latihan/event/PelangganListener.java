6d4fadeeead0411b50af2db4aa2f0572566e19b1
Dida Handian <handiandida@gmail.com>
2018-12-17 20:47:03 +0700

    LatihanMVC

---
+++ src/edu/dida/latihan/event/PelangganListener.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.dida.latihan.event;
+
+import edu.dida.latihan.model.PelangganModel;
+
+/**
+ *
+ * @author Lenovo
+ * Nama  : Dida Handian
+ * NIM   : 101171091
+ * KELAS : IF 3 / PBO3
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+    
+}
---
+++ src/latihanmvc/LatihanMVC.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanmvc;
+
+/**
+ *
+ * @author Lenovo
+ * Nama  : Dida Handian
+ * NIM   : 101171091
+ * KELAS : IF 3 / PBO3
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
